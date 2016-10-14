214d36d3b16eb9f8cd040dac885fa91e75172c5d
whirly <xieriver@live.com>
2016-10-14 17:48:21 +0800

    初步响应式构建完毕
    常用断点设置好了，但是article标签宽度在600px之后就不在变化了，暂时不知道原因，待解决

--- js/index.js
+++ js/index.js
@@ -5,3 +5,28 @@ function loadScript(src) {
 	document.body.appendChild(script);
 }
 
+$(function () {
+	$('#toggle').click(function(event) {
+		$(this).toggleClass('toggle-animate');
+		if($('nav ul.navbar').css('opacity') == '1'){
+			$('.mask').animate({'opacity': 0, 'z-index': '-1'}, 'fast', 'swing', 
+				function () {
+					$('nav ul.navbar').animate({right:'100%', opacity: '0'}, 'normal', 'linear');
+				}
+			);
+		}else{
+			$('nav ul.navbar').animate({right:'0', opacity: '1'}, 'normal', 'linear',
+				 function () {
+					$('.mask').animate({'opacity': 1,'z-index': '1'}, 10, 'swing')
+				}
+			);
+		}
+	});
+	$('.mask').click(function(event) {
+		$(this).animate({'opacity': 0, 'z-index': '-1'}, 'fast', 'swing', 
+				function () {
+					$('nav ul.navbar').animate({right:'100%', opacity: '0'}, 'normal', 'linear');
+				}
+			);
+	});
+})
